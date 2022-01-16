ad0502fa7cd2a4eb5a9c13ad190e5292ba32662d
ZihongQu <zihongqu@gmail.com>
2022-01-16 15:38:41 +0800

    part 1

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ import cors from 'cors';
 import postsRoutes from './routes/posts.js';
 
 const app = express()
-app.use('/posts', postsRoutes); // Meaning every routes in posts.js can only be accessed with /posts prefix
 app.use(bodyParser.json({ limit : "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit : "30mb", extended: true }));
 app.use(cors());
 const port = 5000
 const URL ='mongodb+srv://zihong:wsAD1998@cluster0.r0hgz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+app.use('/posts', postsRoutes); // Meaning every routes in posts.js can only be accessed with /posts prefix
 
 mongoose.connect(URL, {useNewUrlParser:true, useUnifiedTopology:true})
     .then(() => {
